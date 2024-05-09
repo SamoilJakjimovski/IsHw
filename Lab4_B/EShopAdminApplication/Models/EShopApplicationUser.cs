27b0bb82e19db67a84b902859a4b696e3bc95e49
SamoilJakjimovski <153727426+SamoilJakjimovski@users.noreply.github.com>
2024-05-09 09:19:23 +0200

    Initial

---
+++ Lab4_B/EShopAdminApplication/Models/EShopApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Net.Sockets;
+
+namespace EShopAdminApplication.Models
+{
+    public class EShopApplicationUser
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Lab4_B/EShopAdminApplication/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace EShopAdminApplication.Models
+{
+    public class Order 
+    {
+        public Guid Id { get; set; }
+        public string? OwnerId { get; set; }
+        public EShopApplicationUser? Owner { get; set; }
+        public IEnumerable<TicketInOrder>? ProductInOrders { get; set; }
+    }
+}
